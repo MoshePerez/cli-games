ab2e305c94876baf6085f17172c1c21fda9e41f9
MoshePerez <moshe473@gmail.com>
2022-07-08 01:52:21 +0300

    color_game improvments dynamic bottle size detecting when game is lost

--- color_game/color_game.py
+++ color_game/color_game.py
@@ -6,6 +6,7 @@ import random
 from colorama import Back, Style
 
 
+BOTTLE_HIGHT = 5
 YOU_WIN_BANNER = """
 ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗    ██╗██╗███╗   ██╗██╗
 ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║    ██║██║████╗  ██║██║
@@ -14,6 +15,16 @@ YOU_WIN_BANNER = """
    ██║   ╚██████╔╝╚██████╔╝    ╚███╔███╔╝██║██║ ╚████║██╗
    ╚═╝    ╚═════╝  ╚═════╝      ╚══╝╚══╝ ╚═╝╚═╝  ╚═══╝╚═╝ 
 """
+YOU_LOSE_BANNER = """
+██╗   ██╗ ██████╗ ██╗   ██╗    ██╗      ██████╗ ███████╗███████╗██╗
+╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║     ██╔═══██╗██╔════╝██╔════╝██║
+ ╚████╔╝ ██║   ██║██║   ██║    ██║     ██║   ██║███████╗█████╗  ██║
+  ╚██╔╝  ██║   ██║██║   ██║    ██║     ██║   ██║╚════██║██╔══╝  ╚═╝
+   ██║   ╚██████╔╝╚██████╔╝    ███████╗╚██████╔╝███████║███████╗██╗
+   ╚═╝    ╚═════╝  ╚═════╝     ╚══════╝ ╚═════╝ ╚══════╝╚══════╝╚═╝
+"""
+
+
 
 class Color(Enum):
     RED = Back.RED
@@ -43,11 +54,11 @@ class Bottle:
 
     @staticmethod
     def is_color_list_valid(color_list):
-        if len(color_list) != 4:
+        if len(color_list) != BOTTLE_HIGHT:
             print("bad length")
         if not all(isinstance(color, Color) or (color is None) for color in color_list):
             print('bad types')
-        if len(color_list) == 4 and all(isinstance(color, Color) or (color is None) for color in color_list):
+        if len(color_list) == BOTTLE_HIGHT and all(isinstance(color, Color) or (color is None) for color in color_list):
             prev_is_none = False
             for color in color_list:
                 if color is None:
@@ -58,12 +69,7 @@ class Bottle:
         return False
     
     def __str__(self) -> str:
-        return (
-            f"|{colored_space(self[3], 3)}|\n"
-            f"|{colored_space(self[2], 3)}|\n"
-            f"|{colored_space(self[1], 3)}|\n"
-            f"|{colored_space(self[0], 3)}|"
-            )
+        return '\n'.join([f"|{colored_space(color, 3)}|" for color in self._colors_list[::-1]])
 
     def is_full(self):
         return self[-1] is not None
@@ -78,7 +84,7 @@ class Bottle:
         return self._colors_list.count(None)
 
     def pop_color_unit(self):
-        for i in list(range(3, -1, -1)):
+        for i in list(range(BOTTLE_HIGHT -1, -1, -1)):
             if self[i] is not None:
                 color = self[i]
                 self._colors_list[i] = None
@@ -86,7 +92,7 @@ class Bottle:
         raise IndexError("pop from empty bottle")
 
     def push_color_unit(self, color_unit: Color):
-        for i in range(4):
+        for i in range(BOTTLE_HIGHT):
             if self[i] is None:
                 self._colors_list[i] = color_unit
                 return
@@ -128,13 +134,13 @@ class Bottle:
 
 def get_random_mixed_bottles(bottle_count) -> List[Bottle]:
     all_colors = list(Color)
-    colors_to_use = random.sample(all_colors, k=bottle_count) * 4
+    colors_to_use = random.sample(all_colors, k=bottle_count) * BOTTLE_HIGHT
     random.shuffle(colors_to_use)
-    # make sure there are noe sequences of length 4
+    # make sure there are noe sequences of length BOTTLE_HIGHT
     prev_color = None
     count = 0
     for color in colors_to_use:
-        if count == 4:
+        if count == BOTTLE_HIGHT:
             return get_random_mixed_bottles(bottle_count)
         if prev_color == color:
             count += 1
@@ -142,19 +148,51 @@ def get_random_mixed_bottles(bottle_count) -> List[Bottle]:
             prev_color = color
             count = 0
     
-    colors_lists = [colors_to_use[x:x+4] for x in range(0, len(colors_to_use), 4)]
+    colors_lists = [colors_to_use[x:x+BOTTLE_HIGHT] for x in range(0, len(colors_to_use), BOTTLE_HIGHT)]
     return [Bottle(color_list) for color_list in colors_lists]
 
 
 class Game:
     def __init__(self, full_count: int = 7, empty_count: int = 2) -> None:
         full_bottles = get_random_mixed_bottles(full_count)
-        empty_bottles = [Bottle([None, None, None, None]) for _ in range(empty_count)]
+        empty_bottles = [Bottle([None] * BOTTLE_HIGHT) for _ in range(empty_count)]
         self.bottles = full_bottles + empty_bottles
 
-    def is_game_over(self):
+    def is_game_won(self):
         return all((bottle.is_complete() or bottle.is_empty()) for bottle in self.bottles)
     
+    def is_game_lost(self):
+        if not any(bottle.is_empty() for bottle in self.bottles):
+            valid_sources = [bottle for bottle in self.bottles if not bottle.is_complete()]
+            valid_targets = [bottle for bottle in self.bottles if not bottle.is_full()]
+            valid_sources_dict = {}
+            valid_targets_dict = {}
+            for source in valid_sources:
+                top_color, top_layer_size = source.get_top_color_and_size()
+                empty_space = source.get_empty_space_count()
+                valid_sources_dict[source] = {
+                    "top_color": top_color, "top_layer_size": top_layer_size, "empty_space": empty_space
+                }
+            for target in valid_targets:
+                top_color, top_layer_size = target.get_top_color_and_size()
+                empty_space = target.get_empty_space_count()
+                valid_targets_dict[target] = {
+                    "top_color": top_color, "top_layer_size": top_layer_size, "empty_space": empty_space
+                }
+            
+            for source, source_info in valid_sources_dict.items():
+                available_targets = [
+                    target for target, target_info in valid_targets_dict.items()
+                    if source != target and target_info['top_color'] == source_info['top_color']
+                    and target_info['empty_space'] >= source_info['top_layer_size']
+                ]
+                if available_targets:
+                    return False
+            else:
+                return True
+
+        return False
+
     def print_bottles(self):
         out_list = self.bottles[0].__str__().split('\n')
         for bottle in self.bottles[1:]:
@@ -178,8 +216,11 @@ class Game:
     def run(self):
         while True:
             self.print_bottles()
-            if self.is_game_over():
+            if self.is_game_won():
                 print(YOU_WIN_BANNER)
+                return
+            if self.is_game_lost():
+                print(YOU_LOSE_BANNER)
                 return
             source_bottle = self.get_bottle_from_input(prompt="Enter source bottle number:\n")
             target_bottle = self.get_bottle_from_input(prompt="Enter target bottle number:\n")
